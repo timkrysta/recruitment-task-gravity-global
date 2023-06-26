400ef5e4f98f87267d5598e56d1bc3ea019cc5ef
timkrysta <krystatymoteusz@gmail.com>
2023-06-26 14:06:30 +0200

    Created a UserService class with empty methods that will handle the logic of the API

---
+++ backend/src/Services/UserService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Timkrysta\GravityGlobal\Services;
+
+class UserService
+{
+    public static function createNewUser()
+    {
+        
+    }
+    
+    public static function deleteUserById(int $userId)
+    {
+        
+    }
+    
+    public static function getAllUsers()
+    {
+        
+    }
+}
