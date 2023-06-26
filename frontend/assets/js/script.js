6dcb7b49e789e1b15a0c82dd2ae0ca76d55d5c1a
timkrysta <krystatymoteusz@gmail.com>
2023-06-26 15:20:54 +0200

    Created empty html table element in HTML and wrote JavaScript methods that will render it dynamically

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,3 +1,64 @@
-// Add your custom scripts here
+const API_BASE = 'http://localhost:8080/api';
 
-console.log('Good luck 👌');
+const createTableHead = (headers) => {
+  const thead = document.createElement('thead');
+  const headerRow = document.createElement('tr');
+
+  headers.forEach(header => {
+    const th = document.createElement('th');
+    th.textContent = header;
+    headerRow.appendChild(th);
+  });
+
+  thead.appendChild(headerRow);
+  return thead;
+};
+
+const createTableBody = (headers, users) => {
+  const tbody = document.createElement('tbody');
+
+  users.forEach(user => {
+    const row = document.createElement('tr');
+
+    headers.forEach(header => {
+      const cell = document.createElement('td');
+      cell.textContent = user[header];
+      row.appendChild(cell);
+    });
+
+    tbody.appendChild(row);
+  });
+  
+  return tbody;
+};
+
+const getUsersData = async () => {
+  const url = API_BASE + '/users/getAll.php';
+
+  try {
+    const response = await fetch(url);
+    const users = await response.json();
+    return users;
+  } catch (error) {
+    console.log('Error:', error);
+    throw error;
+  }
+};
+
+const renderUsersTable = async () => {
+  try {
+    const users = await getUsersData();
+    const table = document.getElementById('users');
+      
+    const headers = Object.keys(users[0]);
+    const thead = createTableHead(headers);
+    table.appendChild(thead);
+
+    const tbody = createTableBody(headers, users);
+    table.appendChild(tbody);
+  } catch (error) {
+    console.log('Error:', error);
+  }
+};
+
+renderUsersTable();
