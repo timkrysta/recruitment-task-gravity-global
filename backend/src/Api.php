81bc8cb81209bece111b0445f56f69d7e506f7e5
timkrysta <krystatymoteusz@gmail.com>
2023-06-26 16:35:43 +0200

    Added an API wrapper class with a method that restricts what HTTP request methods are supported by a particular endpoint

---
+++ backend/src/Api.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Timkrysta\GravityGlobal;
+
+class Api
+{    
+    /**
+     * Exits when a request method is different than $supportedMethods.
+     *
+     * @param  string[] $supportedMethods
+     * @return void
+     */
+    public static function exitIfRequestMethodNotSupported(array $supportedMethods): void
+    {
+        if (in_array($_SERVER['REQUEST_METHOD'], $supportedMethods, true)) {
+            return;
+        }
+
+        Response::json([
+            'message' => 'Method Not Allowed. This route supports only ' . implode(', ', $supportedMethods) . '.'
+        ], 405);
+    }
+}
