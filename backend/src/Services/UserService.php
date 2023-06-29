df1ab4ebd062dd55ba20ce2c738b4b5be7bf1edc
timkrysta <krystatymoteusz@gmail.com>
2023-06-29 15:30:16 +0200

    Made basic logic related to handling saving of a new user

--- backend/src/Services/UserService.php
+++ backend/src/Services/UserService.php
@@ -7,9 +7,38 @@ use Timkrysta\GravityGlobal\Models\User;
 
 class UserService
 {
-    public static function createNewUser()
+    public static function createNewUser(): bool
     {
-        
+        Api::exitIfRequestMethodNotSupported(['POST']);
+
+        $lastId = User::getLatestUserId();
+
+        User::create([
+            'id' => $lastId + 1,
+            'name' => $_POST['name'],
+            'username' => $_POST['username'],
+            'email' => $_POST['email'],
+            'phone' => $_POST['phone'],
+            'address' => $_POST['address'],
+            'company' => $_POST['company'],
+            /* 'address' => [
+                'street' => "Kulas Light",
+                'suite' => "Apt. 556",
+                'city' => "Gwenborough",
+                'zipcode' => "92998-3874",
+                "geo" => [
+                    "lat" => "-37.3159",
+                    "lng" => "81.1496",
+                ],
+            ],
+            'company' => [
+                'name' => "Romaguera-Crona",
+                'catchPhrase' => "Multi-layered client-server neural-net",
+                'bs' => "harness real-time e-markets",
+            ], */
+        ]);
+
+        return true;
     }
     
     public static function deleteUserById(): bool
